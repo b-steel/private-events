9737b49feee25b2121a0575c7d35b104c34e4f07
Ben Steel <bsteel300@gmail.com>
2020-11-22 20:03:07 -0800

    change url semantics

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import NewUser, UserDetailView, LoginView, LogoutView
+from accounts import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('new/', NewUser.as_view(), name='signup'),
-    path('user/<slug:username>/', UserDetailView.as_view(), name='user_details'), 
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('new/', views.NewUser.as_view(), name='signup'),
+    path('user/<slug:username>/', views.UserDetailView.as_view(), name='user_details'), 
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout')
 ]
