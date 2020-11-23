cb3259bfc370ad372dd270d120fcf68eb3dcace9
Ben Steel <bsteel300@gmail.com>
2020-11-22 20:02:32 -0800

    Add event detail view

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from core import views
 
 app_name='core'
 urlpatterns = [
-    
+    # path('event/index/', PLACEHOLDER, name='event_index'),
+    # path('event/create_event/', PLACEHOLDER, name='create_event'),
+    # path('event/<slug:event_id>/show/', PLACEHOLDER, name='event_details'),
+    # path('event/<slug:event_id>/edit/', PLACEHOLDER, name='edit_event'),
+    # path('event/<slug:event_id>/stage2/', PLACEHOLDER, name='event_stage_two'),
+    # path('get_potential_invites/', PLACEHOLDER, name='get_potential_invites'), 
+    # path('get_potential_hosts/', PLACEHOLDER, name='get_potential_hosts'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404()
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from core import models
 
-# Create your views here.
+class EventDetailView(LoginRequiredMixin, View):
+
+    def get(self, request, id):
+        event = get_object_or_404(models.Event, id=id)
+        return render(request, 'core/event_details.html', {'event': event})
+
